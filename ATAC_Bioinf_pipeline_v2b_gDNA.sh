2e0c362cbdb4a1c23834063ca0dbf7d2275c4377
tmehta12 <tarangmehta80@hotmail.com>
2020-05-07 15:57:51 +0100

    07/05/2020 - Complete; ready to Test

--- ATAC_Bioinf_pipeline_v2b_gDNA.sh
+++ ATAC_Bioinf_pipeline_v2b_gDNA.sh
@@ -87,8 +87,8 @@ rawreaddir=($WD/0.rawreads) # assign raw reads dir
 trimdir=($WD/1.adaptor_trimming) # assign trimmed reads dir
 trimarray=0-0 # INSERT the number range of paired *fastq.merged.gz to trim in zero base e.g. 10 pairs = 0-9
 libids=($scripts/libids.txt) # 2-col space-delimited file where col1 is the *_{R1,R2}.fastq.merged.gz and col2 is the species_tissueID_experiment_barcode_{R1,R2}.fastq.merged.gz
-libids1=libids1.txt
-libids2=libids.sh
+libids1=($trimdir/libids1.txt) # this has the new trimmed ID gzipped fastq file paths
+libids2=($trimdir/libids.sh)
 
 ### 2. Read alignment
 readalign=($WD/2.read_alignment)
@@ -145,6 +145,11 @@ JOBID1=$( sbatch -W --array=$trimarray 1a.trimadaptors.sh | awk '{print $4}' ) #
 # echo 'PRO1563_S1_lib_TCGCCTGC-AACCGCCA_L001_R1.fastq.merged.gz Pn1_T_gDNA_TCGCCTGC-AACCGCCA_L001_R1.fastq.merged.gz' > libids.txt
 # echo 'PRO1563_S1_lib_TCGCCTGC-AACCGCCA_L001_R2.fastq.merged.gz Pn1_T_gDNA_TCGCCTGC-AACCGCCA_L001_R2.fastq.merged.gz' >> libids.txt
 
+# The trimmed files need calling to rename, not the original files!
+PRO1563_S1_lib_CAGAATGC-TAACTCTA_L001_R1.fastq.merged.gz_trimmed.fq.gz
+PRO1563_S1_lib_CAGAATGC-TAACTCTA_L001_R2.fastq.merged.gz_trimmed.fq.gz
+
+
 echo '#!/bin/bash -e' > 1b.renamefiles.sh
 echo '#SBATCH -p tgac-short # partition (queue)' >> 1b.renamefiles.sh
 echo '#SBATCH -N 1 # number of nodes' >> 1b.renamefiles.sh
@@ -156,7 +161,7 @@ echo "#SBATCH --mail-user=$email # send-to address" >> 1b.renamefiles.sh
 echo '#SBATCH -o slurm.%N.%j.out # STDOUT' >> 1b.renamefiles.sh
 echo '#SBATCH -e slurm.%N.%j.err # STDERR' >> 1b.renamefiles.sh
 printf '\n' >> 1b.renamefiles.sh
-echo "grep $spID $libids > $libids1 # grep the relevant species files from the long list" >> 1b.renamefiles.sh
+echo "grep $spID $libids | sed 's/.fastq.merged.gz/.fastq.merged.gz_trimmed.fq.gz/g' > $libids1 # grep the relevant species files from the long list" >> 1b.renamefiles.sh
 echo "sed 's/^/mv /g' $libids1 > $libids2" >> 1b.renamefiles.sh
 echo "sed -i '1 i\\n' $libids2" >> 1b.renamefiles.sh
 echo "sed -i '1 i\#!/bin/sh' $libids2" >> 1b.renamefiles.sh
@@ -223,7 +228,7 @@ printf '\n' >> 2b.readalign.sh
 echo 'ml bowtie2/2.2.6' >> 2b.readalign.sh
 echo 'ml samtools/1.7' >> 2b.readalign.sh
 printf '\n' >> 2b.readalign.sh
-echo "awk -F' ' '{print \$2}' "$libids1 " > " $reads >> 2b.readalign.sh
+echo "awk -F' ' '{print \$2}' $libids1 | sed -e"' "s|^|'$trimdir'/|g" > '"$reads" >> 2b.readalign.sh
 echo 'mapfile -t reads < '$reads' # ${reads[0]} calls read1 AND ${reads[1]} calls read2' >> 2b.readalign.sh
 echo "awk -F' ' '{print \$2}' " $libids1 " | awk -F'_' '{print \$1\"_\"\$2\"_\"\$3}' > "$prefix "# create a prefix file to iterate" >> 2b.readalign.sh
 echo 'mapfile -t prefixmap < '$prefix '# assign prefixes to $prefixmap' >> 2b.readalign.sh
