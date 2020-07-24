7a4373a93198f56617e96602e734daaed6e13659
tmehta12 <tarangmehta80@hotmail.com>
2020-07-24 16:34:34 +0100

    24/07/2020 Commit - Complete

--- meme2jaspar.R
+++ meme2jaspar.R
@@ -6,7 +6,7 @@
 # Decsription: converts meme format PWM to JASPAR 2016 PFM format
 ##########################################################################################################
 
-# setwd("~/Documents/TGAC/Projects/Cichlid_GRNs/Arboretum_GT_v3/1.TFBSs_v2/FINAL_cichlidPWM_motifs/MouseDerived/CS/ab")
+setwd("~/Documents/TGAC/Projects/Cichlid_GRNs/Arboretum_GT_v3/1.TFBSs_v2/FINAL_cichlidPWM_motifs/MouseDerived/CS/ab")
 
 # if (!requireNamespace("BiocManager", quietly=TRUE))
 #   install.packages("BiocManager")
@@ -26,6 +26,17 @@ opt = parse_args(OptionParser(option_list=option_list))
 
 memepwm = read_meme(opt$i)
 write_jaspar(memepwm, opt$o)
+
+# # this will plot the logo and output png format
 # test1 <- read_meme("RG-cich_GTRDdata_mouse_sites_TF_ig_ab.gene.s0.84.ig_1_CS_ab.meme")
 # write_jaspar(test1, 'test.pwm')
+# 
+# test1plot<- view_motifs(test1, use.type = "ICM", method = "ALLR", tryRC = TRUE,
+#             min.overlap = 6, min.mean.ic = 0.25, relative_entropy = FALSE,
+#             normalise.scores = FALSE, min.position.ic = 0, score.strat = "sum",
+#             return.raw = FALSE, dedup.names = FALSE)
+# png('~/Documents/TGAC/Projects/Cichlid_GRNs/Arboretum_GT_v3/1.TFBSs_v2/FINAL_cichlidPWM_motifs/MouseDerived/CS/RG-cich_GTRDdata_mouse_sites_TF_ig_ab.gene.s0.84.ig_1_CS_ab.png')
+# test1plot
+# dev.off()
+
 
